wahjam/wahjam

commit 71503ffdde2cd15f835f2acc968ce65105a27774

Replace JNL_Connection with QTcpSocket
Convert the client connections from JNL_Connection to QTcpSocket.  This
is just the first step and still relies on polling.

The next step is to use signals/slots for event-driven I/O and
disconnect handling.

Note: after disconnect the remote address is no longer available and the
server will print an empty IP address in its log.  This will be fixed
once signals/slots are used for disconnect handling.

Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

ninjam/njclient.cpp
@@ -26,6 +26,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "njclient.h"
 #include "mpb.h"
 #include "../WDL/pcmfmtcvt.h"
@@ -639,10 +641,10 @@ void NJClient::Connect(char *host, char *user, char *pass)
     port=atoi(++p);
     if (!port) port=NJ_PORT;
   }
-  JNL_Connection *c=new JNL_Connection(JNL_CONNECTION_AUTODNS,65536,65536);
-  c->connect(tmp.Get(),port);
-  m_netcon = new Net_Connection;
-  m_netcon->attach(c);
+
+  QTcpSocket *sock = new QTcpSocket;
+  sock->connectToHost(tmp.Get(), port);
+  m_netcon = new Net_Connection(sock);
 
   m_status=0;
 }

ninjam/netmsg.cpp
@@ -19,8 +19,7 @@
 /*
 
   This file provides the implementations of the Net_Messsage class, and 
-  Net_Connection class (handles sending and receiving Net_Messages to
-  a JNetLib JNL_Connection).
+  Net_Connection class (handles sending and receiving Net_Messages)
 
 */
 
@@ -90,130 +89,68 @@ int Net_Message::makeMessageHeader(void *data) // makes message header, data sho
 
 Net_Message *Net_Connection::Run(int *wantsleep)
 {
-  if (!m_con || m_error) return 0;
-
-  {
-    int s=0,r=0;
-    m_con->run(-1,-1,&s,&r);
-    if (wantsleep && (s||r)) *wantsleep=0;
+  if (m_error) {
+    return 0;
   }
 
-  time_t now=time(NULL);
+  time_t now = time(NULL);
 
-  if (m_sendq.Available() > 0) m_last_send=now;
-  else if (now > m_last_send + m_keepalive)
-  {
-    Net_Message *keepalive= new Net_Message;
+  if (now > m_last_send + m_keepalive) {
+    Net_Message *keepalive = new Net_Message;
     keepalive->set_type(MESSAGE_KEEPALIVE);
     keepalive->set_size(0);
     Send(keepalive);
-    m_last_send=now;
+    m_last_send = now;
   }
 
-  // handle sending
-  while (m_con->send_bytes_available()>64 && m_sendq.Available()>0)
-  {
-    Net_Message **topofq = (Net_Message **)m_sendq.Get();
+  Net_Message *retv = 0;
 
-    if (!topofq) break;
-    Net_Message *sendm=*topofq;
-    if (sendm)
-    {
-      if (wantsleep) *wantsleep=0;
-      if (m_msgsendpos<0) // send header
-      {
-        char buf[32];
-        int hdrlen=sendm->makeMessageHeader(buf);
-        m_con->send_bytes(buf,hdrlen);
-
-        m_msgsendpos=0;
-      }
-
-      int sz=sendm->get_size()-m_msgsendpos;
-      if (sz < 1) // end of message, discard and move to next
-      {
-        sendm->releaseRef();
-        m_sendq.Advance(sizeof(Net_Message*));
-        m_msgsendpos=-1;
-      }
-      else
-      {
-        int avail=m_con->send_bytes_available();
-        if (sz > avail) sz=avail;
-        if (sz>0)
-        {
-          m_con->send_bytes((char*)sendm->get_data()+m_msgsendpos,sz);
-          m_msgsendpos+=sz;
-        }
-      }
-    }
-    else
-    {
-      m_sendq.Advance(sizeof(Net_Message*));
-      m_msgsendpos=-1;
-    }
-    {
-      int s=0,r=0;
-      m_con->run(-1,-1,&s,&r);
-      if (wantsleep && (s||r)) *wantsleep=0;
-    }
+  if (!m_recvmsg) {
+    m_recvmsg = new Net_Message;
+    m_recvstate = 0;
   }
 
-  m_sendq.Compact();
-
-  Net_Message *retv=0;
-
-  // handle receive now
-  if (!m_recvmsg) 
-  {
-    m_recvmsg=new Net_Message;
-    m_recvstate=0;
-  }
-
-  while (!retv && m_con->recv_bytes_available()>0)
+  while (!retv && m_sock->bytesAvailable())
   {
     char buf[8192];
-    int bufl=m_con->peek_bytes(buf,sizeof(buf));
-    int a=0;
+    int buflen = m_sock->peek(buf, sizeof(buf));
+    int a = 0;
 
     if (!m_recvstate)
     {
-      a=m_recvmsg->parseMessageHeader(buf,bufl);
-      if (a<0)
+      a = m_recvmsg->parseMessageHeader(buf, buflen);
+      if (a < 0)
       {
-        m_error=-1;
+        m_error = -1;
         break;
       }
-      if (a==0) break;
-      m_recvstate=1;
+      if (a == 0) {
+        break;
+      }
+      m_recvstate = 1;
     }
-    int b2=m_recvmsg->parseAddBytes(buf+a,bufl-a);
+    int b2 = m_recvmsg->parseAddBytes(buf + a, buflen - a);
 
-    m_con->recv_bytes(buf,b2+a); // dump our bytes that we used
+    m_sock->read(buf, b2 + a); // dump our bytes that we used
 
-    if (m_recvmsg->parseBytesNeeded()<1)
+    if (m_recvmsg->parseBytesNeeded() < 1)
     {
-      retv=m_recvmsg;
-      m_recvmsg=0;
-      m_recvstate=0;
+      retv = m_recvmsg;
+      m_recvmsg = 0;
+      m_recvstate = 0;
     }
-    if (wantsleep) *wantsleep=0;
+    if (wantsleep) {
+      *wantsleep = 0;
+    }
   }
-
-  {
-    int s=0,r=0;
-    m_con->run(-1,-1,&s,&r);
-    if (wantsleep && (s||r)) *wantsleep=0;
-  }
-
 
   if (retv)
   {
-    m_last_recv=now;
+    m_last_recv = now;
   }
-  else if (now > m_last_recv + m_keepalive*3)
+  else if (now > m_last_recv + m_keepalive * 3)
   {
-    m_error=-3;
+    m_error = -3;
   }
 
   return retv;
@@ -221,102 +158,55 @@ Net_Message *Net_Connection::Run(int *wantsleep)
 
 int Net_Connection::Send(Net_Message *msg)
 {
-  if (msg)
-  {
-    msg->addRef();
-    if (m_sendq.GetSize() < NET_CON_MAX_MESSAGES*(int)sizeof(Net_Message *))
-      m_sendq.Add(&msg,sizeof(Net_Message *));
-    else 
-    {
-      m_error=-2;
-      msg->releaseRef(); // todo: debug message to log overrun error
-      return -1;
-    }
+  if (!msg) {
+    return 0;
+  }
 
-#if 0
-    if (m_con) 
-    {
-      m_con->run();
+  char buf[32];
+  int hdrlen = msg->makeMessageHeader(buf);
+  qint64 ret = m_sock->write(buf, hdrlen);
+  if (ret != hdrlen) {
+    return -1;
+  }
 
-      while (m_con->send_bytes_available()>64 && m_sendq.Available()>0)
-      {
-        Net_Message **topofq = (Net_Message **)m_sendq.Get();
-
-        if (!topofq) break;
-        Net_Message *sendm=*topofq;
-        if (sendm)
-        {
-          if (m_msgsendpos<0) // send header
-          {
-            char buf[32];
-            int hdrlen=sendm->makeMessageHeader(buf);
-            m_con->send_bytes(buf,hdrlen);
-
-            m_msgsendpos=0;
-          }
-
-          int sz=sendm->get_size()-m_msgsendpos;
-          if (sz < 1) // end of message, discard and move to next
-          {
-            sendm->releaseRef();
-            m_sendq.Advance(sizeof(Net_Message*));
-            m_msgsendpos=-1;
-          }
-          else
-          {
-            int avail=m_con->send_bytes_available();
-            if (sz > avail) sz=avail;
-            if (sz>0)
-            {
-              m_con->send_bytes((char*)sendm->get_data()+m_msgsendpos,sz);
-              m_msgsendpos+=sz;
-            }
-          }
-        }
-        else
-        {
-          m_sendq.Advance(sizeof(Net_Message*));
-          m_msgsendpos=-1;
-        }
-        m_con->run();
-      }
-
-      m_sendq.Compact();
-    }
-  #endif 
-
+  ret = m_sock->write((const char*)msg->get_data(), msg->get_size());
+  if (ret != msg->get_size()) {
+    return -1;
   }
   return 0;
 }
 
 int Net_Connection::GetStatus()
 {
-  if (m_error) return m_error;
-  return !m_con || m_con->get_state()<JNL_Connection::STATE_RESOLVING || m_con->get_state()>=JNL_Connection::STATE_CLOSING; // 1 if disconnected somehow
+  if (m_error) {
+    return m_error;
+  }
+  if (!m_sock || !m_sock->isOpen()) {
+    return 1;
+  }
+  switch (m_sock->state()) {
+  case QAbstractSocket::HostLookupState:
+  case QAbstractSocket::ConnectingState:
+  case QAbstractSocket::ConnectedState:
+  case QAbstractSocket::ClosingState:
+    return 0;
+  default:
+    return 1;
+  }
+}
+
+QHostAddress Net_Connection::GetRemoteAddr()
+{
+  return m_sock->peerAddress();
 }
 
 Net_Connection::~Net_Connection()
-{ 
-  Net_Message **p=(Net_Message **)m_sendq.Get();
-  if (p)
-  {
-    int n=m_sendq.Available()/sizeof(Net_Message *);
-    while (n-->0)
-    {
-      (*p)->releaseRef();
-      p++;
-    }
-    m_sendq.Advance(m_sendq.Available());
-    
-  }
-
-  delete m_con; 
+{
   delete m_recvmsg;
-
+  delete m_sock;
 }
 
-
-void Net_Connection::Kill(int quick) 
-{ 
-  m_con->close(); 
+void Net_Connection::Kill()
+{
+  m_sock->close();
 }

ninjam/netmsg.h
@@ -18,9 +18,9 @@
 
 /*
 
-  This header provides the declarations for the Net_Messsage class, and 
-  Net_Connection class (handles sending and receiving Net_Messages to
-  a JNetLib JNL_Connection).
+  This header provides the declarations for the Net_Messsage class, and
+  Net_Connection class (handles sending and receiving Net_Messages)
+
 */
 
 
@@ -28,8 +28,12 @@
 #ifndef _NETMSG_H_
 #define _NETMSG_H_
 
+#include <time.h>
+
+#include <QTcpSocket>
+#include <QHostAddress>
+
 #include "../WDL/queue.h"
-#include "../WDL/jnetlib/jnetlib.h"
 
 #define NET_MESSAGE_MAX_SIZE 16384
 
@@ -83,21 +87,17 @@ class Net_Message
 class Net_Connection
 {
   public:
-    Net_Connection() : m_error(0),m_msgsendpos(-1), m_recvstate(0),m_recvmsg(0),m_con(0)
-    { 
+    Net_Connection(QTcpSocket *sock) : m_error(0), m_recvstate(0), m_recvmsg(0), m_sock(sock)
+    {
       SetKeepAlive(0);
     }
     ~Net_Connection();
 
-    void attach(JNL_Connection *con) 
-    {
-      m_con=con; 
-    }
-
     Net_Message *Run(int *wantsleep=0);
     int Send(Net_Message *msg); // -1 on error, i.e. queue full
     int GetStatus(); // returns <0 on error, 0 on normal, 1 on disconnect
-    JNL_Connection *GetConnection() { return m_con; }
+
+    QHostAddress GetRemoteAddr();
 
     void SetKeepAlive(int interval)
     {
@@ -105,23 +105,16 @@ class Net_Connection
       m_last_send=m_last_recv=time(NULL);
     }
 
-    void Kill(int quick=0);
+    void Kill();
 
   private:
     int m_error;
-
     int m_keepalive;
-    int m_msgsendpos;
-
-    time_t m_last_send, m_last_recv;
-
+    time_t m_last_send;
+    time_t m_last_recv;
     int m_recvstate;
     Net_Message *m_recvmsg;
-
-    JNL_Connection *m_con;
-    WDL_Queue m_sendq;
-
-
+    QTcpSocket *m_sock;
 };
 
 

ninjam/njclient.h
@@ -67,7 +67,6 @@
 #include <time.h>
 #include "../WDL/string.h"
 #include "../WDL/ptrlist.h"
-#include "../WDL/jnetlib/jnetlib.h"
 #include "../WDL/sha.h"
 #include "../WDL/rng.h"
 #include "../WDL/mutex.h"

ninjam/server/Server.cpp
@@ -17,10 +17,12 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#ifdef _WIN32
-#include <io.h>
+#ifndef _WIN32
+#include <arpa/inet.h>
 #endif
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <QTcpSocket>
 
 #include "ninjamsrv.h"
@@ -41,7 +43,6 @@ Server::~Server()
 
 void AccessControlList::add(unsigned long addr, unsigned long mask, int flags)
 {
-  addr = ntohl(addr);
   ACLEntry f = {addr, mask, flags};
   int os = list.GetSize();
   list.Resize(os + sizeof(f));
@@ -55,8 +56,6 @@ void AccessControlList::clear()
 
 int AccessControlList::lookup(unsigned long addr)
 {
-  addr = ntohl(addr);
-
   ACLEntry *p = (ACLEntry *)list.Get();
   int x = list.GetSize() / sizeof(ACLEntry);
   while (x--)
@@ -76,7 +75,8 @@ void Server::enforceACL()
   for (x = 0; x < group->m_users.GetSize(); x ++)
   {
     User_Connection *c=group->m_users.Get(x);
-    if (config->acl.lookup(c->m_netcon.GetConnection()->get_remote()) == ACL_FLAG_DENY)
+    uint32_t addr = c->m_netcon.GetRemoteAddr().toIPv4Address();
+    if (config->acl.lookup(addr) == ACL_FLAG_DENY)
     {
       c->m_netcon.Kill();
       killcnt++;
@@ -124,7 +124,7 @@ void Server::acceptNewConnection()
     return;
   }
 
-  uint32_t addr = htonl(sock->peerAddress().toIPv4Address());
+  uint32_t addr = sock->peerAddress().toIPv4Address();
 
   int flag = config->acl.lookup(addr);
   QString ipstr = sock->peerAddress().toString();
@@ -137,21 +137,7 @@ void Server::acceptNewConnection()
     return;
   }
 
-  JNL_Connection *con = new JNL_Connection(JNL_CONNECTION_AUTODNS, 2*65536, 65536);
-
-  struct sockaddr_in sa;
-  sa.sin_family = AF_INET;
-  sa.sin_port = htons(sock->peerPort());
-  sa.sin_addr.s_addr = htonl(sock->peerAddress().toIPv4Address());
-
-  /* Clone the file descriptor so the QTcpSocket can be safely closed.  This is
-   * necessary so the QTcpSocket does not read incoming data and interfere with
-   * the JNL_Connection.
-   */
-  con->connect(dup(sock->socketDescriptor()), &sa);
-  delete sock;
-
-  group->AddConnection(con, flag == ACL_FLAG_RESERVE);
+  group->AddConnection(sock, flag == ACL_FLAG_RESERVE);
 }
 
 /* Run an iteration of the main loop

ninjam/server/example.cfg
@@ -22,6 +22,7 @@ AllowHiddenUsers no   # set to yes to allow people without channels to not appea
 
 
 #ACL list lets you specify in order a list, first match is used
+ACL 127.0.0.1/8 deny
 ACL 10.0.0.0/8 deny
 ACL 192.168.0.0/16 reserve # reserve slots for local
 ACL 0.0.0.0/0 allow        # allow all

ninjam/server/ninjamsrv.cpp
@@ -311,8 +311,8 @@ static int ConfigOnToken(ServerConfig *config, LineParser *lp)
     if (t)
     {
       *t++=0;
-      unsigned long addr=JNL::ipstr_to_addr(v);
-      if (addr != INADDR_NONE)
+      QHostAddress hostaddr(v);
+      if (hostaddr != QHostAddress::Null)
       {
         int maskbits=atoi(t);
         if (maskbits >= 0 && maskbits <= 32)
@@ -321,8 +321,8 @@ static int ConfigOnToken(ServerConfig *config, LineParser *lp)
           if (flag >= 0)
           {
             suc=1;
-            unsigned long mask=~(0xffffffff>>maskbits);
-            config->acl.add(addr, mask, flag);
+            unsigned long mask = 0xffffffff << maskbits;
+            config->acl.add(hostaddr.toIPv4Address(), mask, flag);
           }
         }
       }

ninjam/server/usercon.cpp
@@ -29,6 +29,9 @@
 #else
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
 #endif
 
 #include <ctype.h>
@@ -96,11 +99,9 @@ static void type_to_string(unsigned int t, char *out)
 
 #define TRANSFER_TIMEOUT 8
 
-User_Connection::User_Connection(JNL_Connection *con, User_Group *grp) : m_auth_state(0), m_clientcaps(0), m_auth_privs(0), m_reserved(0), m_max_channels(0),
+User_Connection::User_Connection(QTcpSocket *sock, User_Group *grp) : m_netcon(sock), m_auth_state(0), m_clientcaps(0), m_auth_privs(0), m_reserved(0), m_max_channels(0),
       m_vote_bpm(0), m_vote_bpm_lasttime(0), m_vote_bpi(0), m_vote_bpi_lasttime(0)
 {
-  m_netcon.attach(con);
-
   WDL_RNG_bytes(m_challenge,sizeof(m_challenge));
 
   mpb_server_auth_challenge ch;
@@ -168,9 +169,6 @@ void User_Connection::SendConfigChangeNotify(int bpm, int bpi)
 
 int User_Connection::OnRunAuth(User_Group *group)
 {
-  char addrbuf[256];
-  JNL::addr_to_ipstr(m_netcon.GetConnection()->get_remote(),addrbuf,sizeof(addrbuf));
- 
   {
     WDL_SHA1 shatmp;
 
@@ -183,7 +181,7 @@ int User_Connection::OnRunAuth(User_Group *group)
 
     if ((m_lookup->reqpass && memcmp(buf,m_lookup->sha1buf_request,WDL_SHA1SIZE)) || !m_lookup->user_valid)
     {
-      logText("%s: Refusing user, invalid login/password\n",addrbuf);
+      logText("%s: Refusing user, invalid login/password\n", m_netcon.GetRemoteAddr().toString().toLocal8Bit().constData());
       mpb_server_auth_reply bh;
       bh.errmsg="invalid login/password";
       Send(bh.build());
@@ -296,7 +294,9 @@ int User_Connection::OnRunAuth(User_Group *group)
     }
     if (cnt >= group->m_max_users)
     {
-      logText("%s: Refusing user %s, server full\n",addrbuf,m_username.Get());
+      logText("%s: Refusing user %s, server full\n",
+              m_netcon.GetRemoteAddr().toString().toLocal8Bit().constData(),
+              m_username.Get());
       // sorry, gotta kill this connection
       mpb_server_auth_reply bh;
       bh.errmsg="server full";
@@ -307,7 +307,9 @@ int User_Connection::OnRunAuth(User_Group *group)
 
 
 
-  logText("%s: Accepted user: %s\n",addrbuf,m_username.Get());
+  logText("%s: Accepted user: %s\n",
+          m_netcon.GetRemoteAddr().toString().toLocal8Bit().constData(),
+          m_username.Get());
 
   {
     mpb_server_auth_reply bh;
@@ -408,9 +410,8 @@ int User_Connection::Run(User_Group *group, int *wantsleep)
       if (time(NULL) > m_connect_time+120) // if we haven't gotten an auth reply in 120s, disconnect. The reason this is so long is to give
                                         // the user time to potentially read the license agreement.
       {
-        char buf[256];
-        JNL::addr_to_ipstr(m_netcon.GetConnection()->get_remote(),buf,sizeof(buf));
-        logText("%s: Got an authorization timeout\n",buf);
+        logText("%s: Got an authorization timeout\n",
+                m_netcon.GetRemoteAddr().toString().toLocal8Bit().constData());
         m_connect_time=time(NULL)+120;
         mpb_server_auth_reply bh;
         bh.errmsg="authorization timeout";
@@ -440,10 +441,9 @@ int User_Connection::Run(User_Group *group, int *wantsleep)
       mpb_server_auth_reply bh;
       bh.errmsg=tab[err_st-1];
 
-      char addrbuf[256];
-      JNL::addr_to_ipstr(m_netcon.GetConnection()->get_remote(),addrbuf,sizeof(addrbuf));
-
-      logText("%s: Refusing user, %s\n",addrbuf,bh.errmsg);
+      logText("%s: Refusing user, %s\n",
+              m_netcon.GetRemoteAddr().toString().toLocal8Bit().constData(),
+              bh.errmsg);
 
       Send(bh.build());
       m_netcon.Run();
@@ -463,9 +463,7 @@ int User_Connection::Run(User_Group *group, int *wantsleep)
 
     if (m_lookup)
     {
-      char addrbuf[256];
-      JNL::addr_to_ipstr(m_netcon.GetConnection()->get_remote(),addrbuf,sizeof(addrbuf));
-      m_lookup->hostmask.Set(addrbuf);
+      m_lookup->hostmask.Set(m_netcon.GetRemoteAddr().toString().toLocal8Bit().constData());
       memcpy(m_lookup->sha1buf_request,authrep.passhash,sizeof(m_lookup->sha1buf_request));
     }
 
@@ -946,9 +944,9 @@ int User_Group::Run()
             if (mfmt_changes) Broadcast(mfmt.build(),p);
           }
 
-          char addrbuf[256];
-          JNL::addr_to_ipstr(p->m_netcon.GetConnection()->get_remote(),addrbuf,sizeof(addrbuf));
-          logText("%s: disconnected (username:'%s', code=%d)\n",addrbuf,p->m_auth_state>0?p->m_username.Get():"",ret);
+          logText("%s: disconnected (username:'%s', code=%d)\n",
+                  p->m_netcon.GetRemoteAddr().toString().toLocal8Bit().constData(),
+                  p->m_auth_state > 0 ? p->m_username.Get() : "", ret);
 
           delete p;
           m_users.Delete(thispos);
@@ -971,10 +969,12 @@ void User_Group::SetConfig(int bpi, int bpm)
   Broadcast(mk.build());
 }
 
-void User_Group::AddConnection(JNL_Connection *con, int isres)
+void User_Group::AddConnection(QTcpSocket *sock, int isres)
 {
-  User_Connection *p = new User_Connection(con, this);
-  if (isres) p->m_reserved=1;
+  User_Connection *p = new User_Connection(sock, this);
+  if (isres) {
+    p->m_reserved = 1;
+  }
   m_users.Add(p);
 }
 

commit 5ea312eb382f0be62ebbe4552096b2d4bc11a3cc

Switch from unlink(2) to remove(3) in njclient.cpp
The build fails on non-Windows platforms due to a unlink(2) call in
njclient.cpp without including unistd.h.  It is not clear why this build
failure never appeared before but it was introduced in commit
71503ffdde2cd15f835f2acc968ce65105a27774 "Replace JNL_Connection with
QTcpSocket" when the jnetlib header was no longer included.

Since njclient.cpp already includes stdio.h, use the standard C
remove(3) function instead of POSIX unlink(2).  This way we avoid more
platform-specific #ifdefs.

Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

ninjam/njclient.cpp
@@ -66,7 +66,7 @@ class DecodeState
 #ifdef _WIN32
         DeleteFile(delete_on_delete.Get());
 #else
-        unlink(delete_on_delete.Get());
+        remove(delete_on_delete.Get());
 #endif
       }
     }