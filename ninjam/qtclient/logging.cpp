71853ea4334ed7a301954e5da7e66513d4fa7035
Stefan Hajnoczi <stefanha@gmail.com>
2012-07-03 16:25:54 +0100

    Log system information on startup
    Log operating system information and the Qt version.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

--- ninjam/qtclient/logging.cpp
+++ ninjam/qtclient/logging.cpp
@@ -18,6 +18,7 @@
 
 #include <stdio.h>
 #include <QDateTime>
+#include <QSysInfo>
 
 static FILE *logfp;
 
@@ -48,6 +49,79 @@ static void logMsgHandler(QtMsgType type, const char *msg)
   fprintf(logfp, "%s %s: %s\n", timestamp.toUtf8().data(), typestr, msg);
 }
 
+/* Called at startup */
+static void logSystemInformation()
+{
+  qDebug("CPU architecture %zu-bit %s", QSysInfo::WordSize,
+         QSysInfo::ByteOrder == QSysInfo::LittleEndian ?
+         "little-endian" : "big-endian");
+
+#if defined(Q_WS_WIN)
+  switch (QSysInfo::windowsVersion()) {
+  case QSysInfo::WV_2000:
+    qDebug("Windows 2000");
+    break;
+  case QSysInfo::WV_XP:
+    qDebug("Windows XP");
+    break;
+  case QSysInfo::WV_2003:
+    qDebug("Windows 2003");
+    break;
+  case QSysInfo::WV_VISTA:
+    qDebug("Windows Vista");
+    break;
+  case QSysInfo::WV_WINDOWS7:
+    qDebug("Windows 7");
+    break;
+  default:
+    qDebug("Unsupported or unrecognized Windows version");
+    break;
+  }
+#elif defined(Q_WS_MAC)
+  switch (QSysInfo::MacintoshVersion) {
+  case QSysInfo::MV_10_3:
+    qDebug("Mac OS X 10.3");
+    break;
+  case QSysInfo::MV_10_4:
+    qDebug("Mac OS X 10.4");
+    break;
+  case QSysInfo::MV_10_5:
+    qDebug("Mac OS X 10.5");
+    break;
+  case QSysInfo::MV_10_6:
+    qDebug("Mac OS X 10.6");
+    break;
+  case QSysInfo::MV_10_7:
+    qDebug("Mac OS X 10.7");
+    break;
+  default:
+    qDebug("Unsupported or unrecognized Mac OS version");
+    break;
+  }
+#elif defined(Q_OS_LINUX)
+  {
+    FILE *issuefp = fopen("/etc/issue", "r");
+    qDebug("Reading /etc/issue...");
+    if (issuefp) {
+      char buf[BUFSIZ];
+      while (fgets(buf, sizeof(buf), issuefp)) {
+        size_t len = strlen(buf);
+        if (len > 0 && buf[len - 1] == '\n') {
+          buf[len - 1] = '\0';
+          len--;
+        }
+        if (len > 0) {
+          qDebug(buf);
+        }
+      }
+      fclose(issuefp);
+    }
+    qDebug("End of /etc/issue");
+  }
+#endif
+  qDebug("Qt %s", qVersion());
+}
+
 void logInit(const QString &filename)
 {
   logfp = fopen(filename.toUtf8().data(), "w");
@@ -59,4 +133,5 @@ void logInit(const QString &filename)
   qInstallMsgHandler(logMsgHandler);
 
   qDebug("Wahjam %s (%s)", VERSION, COMMIT_ID);
+  logSystemInformation();
 }
